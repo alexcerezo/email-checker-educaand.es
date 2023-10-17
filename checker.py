e17cea1a0ad2586705087e7040ca561e30a35472
Alex Cerezo <alexcerezocontreras@gmail.com>
2023-10-17 13:02:27 +0200

    Update checker.py

--- checker.py
+++ checker.py
@@ -4,6 +4,52 @@ import socket
 import time
 import random
 
+def generar_usuario():
+    nombre = input("Nombre: ")
+    apellido1 = input("Primer apellido: ")
+    apellido2 = input("Segundo apellido: ")
+    inicial = nombre[0]
+    primera_parte = apellido1[:3]
+    segunda_parte = apellido2[:3]
+    usuario = (inicial + primera_parte + segunda_parte).lower()
+    return usuario
+
+def generar_correos():
+    usuario = generar_usuario()
+    with open('emails.txt', 'w') as f:
+        for i in range(1000):
+            correo = usuario + '{:03d}'.format(i) + '@g.educaand.es'
+            f.write(correo + '\n')
+        for dia in range(1, 32):
+            for mes in range(1, 13):
+                if mes == 2 and dia > 29:
+                    continue
+                elif mes in (4, 6, 9, 11) and dia > 30:
+                    continue
+                correo = usuario + '{:02d}{:02d}'.format(dia, mes) + '@g.educaand.es'
+                f.write(correo + '\n')
+    print("Correos exportados correctamente.")
+
+def verificar_correos():
+    with open('emails.txt', 'r') as archivo:
+        contenido = archivo.read().strip()
+        email_list = contenido.split('\n')
+    verificados = []
+    for email in email_list:
+        if check_email(email):
+            print(f"{email} existe")
+            verificados.append(email)
+        else:
+            print(f"{email} no existe")
+    if verificados:
+        with open('Verificados.txt', 'w') as f:
+            for email in verificados:
+                f.write(email + '\n')
+        print("Correos verificados exportados correctamente.")
+    else:
+        print("No se encontraron correos verificados.")
+
+
 def check_email(email):
     # Dividir la dirección de correo electrónico en usuario y dominio
     username, domain = email.split('@')
@@ -28,17 +74,13 @@ def check_email(email):
         time.sleep(random.uniform(1, 3))
         return False
 
-# Nombre del archivo que contiene la lista de correos electrónicos
-nombre_archivo = 'emails.txt'
-
-# Leer el contenido del archivo y dividirlo en una lista de correos electrónicos
-with open(nombre_archivo, 'r') as archivo:
-    contenido = archivo.read().strip()
-    email_list = contenido.split('\n')
-
-# Verificación de cada dirección de correo electrónico en la lista
-for email in email_list:
-    if check_email(email):
-        print(f"{email} existe")
+while True:
+    opcion = input("\nSelecciona una opción:\n1. Generar correos\n2. Verificar correos\n3. Salir\n")
+    if opcion == "1":
+        generar_correos()
+    elif opcion == "2":
+        verificar_correos()
+    elif opcion == "3":
+        break
     else:
-        print(f"{email} no existe")
+        print("Opción no válida. Introduce un número del 1 al 3.")
